c831f3535dcb93af5e7feb58a223ccc900a8c8ac
SimSla <simon.slangen@gmail.com>
2015-04-10 16:58:12 +0200

    setup script limited to installing dependencies

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 
-setup(name='owncloud-screenshots-python',
+setup(name='oc_screenshots',
       version='0.9',
       description='OwnCloud Auto Screenshots Uploader',
       author='Simon Slangen',
-      requires=['pyocclient', 'pyperclip', 'pync', 'watchdog']
+      requires=['pyocclient', 'pyperclip', 'pync', 'watchdog'],
       )
